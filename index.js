d6b6f6874299698ce1a9381eff04743ddc38f15c
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-18 19:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = "I don't do much.";
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  
+  kittens.push(name);
+  return kittens;
 }
 
 function destructivelyPrependKitten() {
-  
+  kittens.pop();
+  return kittens;
 }
 
 function destructivelyRemoveFirstKitten() {
